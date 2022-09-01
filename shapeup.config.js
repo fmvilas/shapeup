026ddcadcbad6fc1580362c01942300b385c6a06
Fran Mendez <fmvilas@gmail.com>
2022-09-01 13:11:09 +0200

    Improve description and fix link to pitch issue

--- shapeup.config.js
+++ shapeup.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tagline: `Visualize the progress that I'm making, one cycle at a time. Want to propose that I work on something? <a href="https://github.com/fmvilas/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.md&title=" target="_blank">Time to pitch!</a>.`,
+  tagline: `Visualize the progress of my work at <a href="https://www.asyncapi.com" target="_blank">AsyncAPI</a>. Want to propose that I work on something? <a href="https://github.com/fmvilas/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.yaml&title=" target="_blank">Time to pitch!</a>.`,
   owner: 'fmvilas',
   projectNumber: 2,
 }
