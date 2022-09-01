09896246f18b6dda666744f809cf1444492712d1
Fran Mendez <fmvilas@gmail.com>
2022-09-01 11:24:12 +0200

    Change description

--- shapeup.config.js
+++ shapeup.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tagline: `Visualize the progress that I'm making, one cycle at a time. Want to propose that I work on some task? <a href="https://github.com/fmvilas/shapeup/issues/new/choose" target="_blank">Create an issue</a>.`,
+  tagline: `Visualize the progress that I'm making, one cycle at a time. Want to propose that I work on something? <a href="https://github.com/fmvilas/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.md&title=" target="_blank">Time to pitch!</a>.`,
   owner: 'fmvilas',
   projectNumber: 2,
 }
