af3f61f2a75ccddc693069578b0b43fe0954553d
Samir AMZANI <samir.amzani@gmail.com>
2023-06-04 22:46:52 +0200

    Feat: Update config sample

--- shapeup.config-sample.js
+++ shapeup.config-sample.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "owner": "Amzani",
-  "projectNumber": 4,
-  "fullname": "Samir AMZANI",
-  "email": "samir.amzani@gmail.com",
+  "owner": "fmvilas",
+  "projectNumber": 2,
+  "fullname": "Fran Méndez",
+  "email": "fmvilas@gmail.com",
   "project": "AsyncAPI",
   "website": "https://www.asyncapi.com",
-  "tagline": "Visualize the progress of my work at <a href=\"https://www.asyncapi.com\" target=\"_blank\">AsyncAPI</a>. Want to propose that I work on something? <a href=\"https://github.com/Amzani/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.yaml&title=\" target=\"_blank\">Time to pitch!</a>"
+  "tagline": "Visualize the progress of my work at <a href=\"https://www.asyncapi.com\" target=\"_blank\">AsyncAPI</a>. Want to propose that I work on something? <a href=\"https://github.com/fmvilas/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.yaml&title=\" target=\"_blank\">Time to pitch!</a>"
 };
