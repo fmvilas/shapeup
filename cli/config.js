0f84772ce6da7f88b603acaeb9225f2de93afa10
Samir AMZANI <samir.amzani@gmail.com>
2023-06-04 22:21:59 +0200

    Feat: Update Readme + cleaning

--- cli/config.js
+++ cli/config.js
@@ -9,12 +9,12 @@ const program = new Command();
 program.version('1.0.0');
 
 program
-  .command('configure')
+  .command('init')
   .description('Configure your ShapeUp dashboard')
   .action(async () => {
     const questions = [
       { type: 'input', name: 'owner', message: 'Enter your Github username:' },
-      { type: 'input', name: 'projectNumber', message: 'Enter project number:' },
+      { type: 'input', name: 'projectNumber', message: 'Enter a project number:' },
       { type: 'input', name: 'fullname', message: 'Enter Your full name:' },
       { type: 'input', name: 'email', message: 'Enter email:' },
       { type: 'input', name: 'project', message: 'Enter your project Name:' },
@@ -25,15 +25,15 @@ program
     const answers = await inquirer.prompt(questions);
 
     const config = {
-      [owner]: answers.owner || '',
-      [projectNumber]: parseInt(answers.projectNumber) || '',
-      [fullname]: answers.fullname || '',
-      [email]: answers.email || '',
-      [project]: answers.project || '',
-      [website]: answers.website || '',
+      owner: answers.owner || '',
+      projectNumber: parseInt(answers.projectNumber) || '',
+      fullname: answers.fullname || '',
+      email: answers.email || '',
+      project: answers.project || '',
+      website: answers.website || '',
     };
 
-    config[tagline] = `Visualize the progress of my work at <a href="${config.website}" target="_blank">${config.project}</a>. Want to propose that I work on something? <a href="https://github.com/${config.owner}/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.yaml&title=" target="_blank">Time to pitch!</a>`;
+    config.tagline = `Visualize the progress of my work at <a href="${config.website}" target="_blank">${config.project}</a>. Want to propose that I work on something? <a href="https://github.com/${config.owner}/shapeup/issues/new?assignees=&labels=Pitch&template=pitch.yaml&title=" target="_blank">Time to pitch!</a>`;
 
     const configFilePath = 'shapeup.config.js';
     fs.writeFileSync(configFilePath, `module.exports = ${JSON.stringify(config, null, 2)};\n`);
@@ -43,5 +43,6 @@ program
 
     console.log('Configuration generated successfully!');
   });
+
 
 program.parse(process.argv);
