889b99428b318b604385f1b215fcdb0a939b90df
Fran Mendez <fmvilas@gmail.com>
2023-06-04 18:50:13 +0200

    Set cursor pointer on button

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -22,10 +22,10 @@ export function Intro() {
       <p className="mt-4 text-sm/6 text-gray-300">
         Convert your GitHub Project into Shape Up one. Visualize the progress of your team while keeping your operations on GitHub.
       </p>
-      <a onClick={() => signIn('github')} className="inline-flex flex-row pointer align-middle mt-8 px-5 py-4 border-sky-700 border rounded-lg hover:border-sky-950 hover:bg-sky-950">
+      <button onClick={() => signIn('github')} className="inline-flex flex-row cursor-pointer align-middle mt-8 px-5 py-4 border-sky-700 border rounded-lg hover:border-sky-950 hover:bg-sky-950">
         <GitHubIcon className="w-6 inline-block mr-4" />
         Sign in using GitHub
-      </a>
+      </button>
     </>
   )
 }
